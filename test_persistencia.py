8dbff646a65f9c8046c8781de7cdc3a639673b7d
oruiz-xpheraco <146000446+oruiz-xpheraco@users.noreply.github.com>
2023-12-11 20:46:03 -0500

    workflow

--- test_persistencia.py
+++ test_persistencia.py
@@ -3,9 +3,9 @@ Pruebas Persistencia
 """
 import persistencia
 def test_guardar_pedido():
- """
- Prueba general
- """
+    """
+    Prueba general
+    """
     with open("pedidos.txt", "w+", encoding="utf-8") as file:
         persistencia.guardar_pedido("Pedro", "Gil de Diego")
         persistencia.guardar_pedido("Michael", "Jordan")
