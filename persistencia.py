533638e0f626f27764bd65a16320161a34b05107
oruiz-xpheraco <146000446+oruiz-xpheraco@users.noreply.github.com>
2023-12-11 21:02:38 -0500

    workflow

--- persistencia.py
+++ persistencia.py
@@ -2,4 +2,4 @@ def guardar_pedido(nombre,apellidos):
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
-
+        
