70825a52ae6e1c1d43007362fc41ee8900b11a2c
oruiz-xpheraco <146000446+oruiz-xpheraco@users.noreply.github.com>
2023-12-11 22:15:23 -0500

    workflow

--- app.py
+++ app.py
@@ -2,12 +2,12 @@
 App
 """
 import json
-from flask import Flask, redirect, request, Response
-from flask_cors import CORS
+from flask import Flask, redirect, request
+# from flask_cors import CORS
 from persistencia import guardar_pedido
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 @app.route("/pizza",methods=['GET', 'POST'])
 def iniciar_pedido():
@@ -22,19 +22,16 @@ def iniciar_pedido():
     return redirect("http://127.0.0.1:5500/solicita_pedido.html", code=302)
 @app.route("/checksize",methods=['POST'])
 def checksize():
-      """
-      Comprueba disponibilidad de un tamaño de pizza.
-      Aquí va el código Python. Debe capturar el parámetro "size" de la request. Debe
-      retornar siempre "Disponible", excepto para el tamaño "S" que debe retornar "No
-      disponible" y se debe asignar en mensaje, así mensaje = "Lo que corresponda"
-      """
-      data = json.loads(request.data)
-      tamanio_pizza = data.get("tamanioPizza",None)
-      mensaje = "No disponible" if tamanio_pizza.lower() == "s" else "Disponible"
-      tamanio_pizza = data.get("tamanioPizza",None)
-      return Response(json.dumps({'mensaje':mensaje}), 200, {'Access-Control-Allow-Origin': '*','ContentType':'application/json'})
-    #return json.dumps({'mensaje':mensaje}), 200, {'ContentType':'application/json'}
-
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    Aquí va el código Python. Debe capturar el parámetro "size" de la request. Debe
+    retornar siempre "Disponible", excepto para el tamaño "S" que debe retornar "No
+    disponible" y se debe asignar en mensaje, así mensaje = "Lo que corresponda"
+    """
+    data = json.loads(request.data)
+    tamanio_pizza = data.get("tamanioPizza",None)
+    mensaje = "No disponible" if tamanio_pizza.lower() == "s" else "Disponible"
+    return json.dumps({'mensaje':mensaje}), 200, {'ContentType':'application/json'}
 def crear_pedido(pedidos):
     """
     crear Pedido
